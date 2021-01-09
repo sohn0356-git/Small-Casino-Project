17c46d4c651fff893c60c2d035606e8e417abc26
hsbummy <morpheus3263@gmail.com>
2021-01-09 18:59:36 +0900

    Modify NumberGuess Game V2

--- Small Casino Project/NumberGuessGame.py
+++ Small Casino Project/NumberGuessGame.py
@@ -11,37 +11,34 @@ def play():
         guess = input('소주병뚜껑 숫자 맞히기 : 1부터 99까지., 숫자를 한 개 입력해보세요~~?, ( q 를 입력하면 게임이 종료됩니다.)')
 
         if guess == "q":
-            print("게임이 종료됩니다.")
+            print("게임을 종료합니다.")
             break
 
-
-        elif int(guess) >= 100 or int(guess) <= 0:
+        elif guess.isdecimal() != True:
             print("잘못된 값을 입력하셨습니다.")
             continue
 
-        '''숫자를 맞추게 되면 1000만원을 획득하고, 틀릴 시에는 500만원을 차감합니다.
-            사용자가 입력한 숫자가  정답보다 클 경우 down을 말해 값을 내리라고 말해줄 것이며
-            사용자가 입력한 숫자가 정답보다 작을 경우 up을 말해 값을 더 키우라고 합니다.
-            총 남은 금액을 합산해 상금으로 획득하게 됩니다.'''
+        guess = int(guess)
 
-
-
+        if guess >= 100 or guess <= 0:
+            print("잘못된 값을 입력하셨습니다.")
+            continue
 
         while True:
 
-            if int(guess) == number:
+            if  guess == number:
                 award += 1000
                 print("{0} 만원을 획득하셨습니다.\nGG\n축하합니다!!!".format(award))
                 break
 
-            if int(guess) > number:
+            if  guess > number:
                 print("DOWN")
                 award -= 500
                 print()
                 print("현재 상금: {0} 만원".format(award))
                 break
 
-            elif int(guess) < number:
+            elif guess < number:
                 print("UP")
                 award -= 500
                 print()
@@ -49,8 +46,6 @@ def play():
                 break
 
             return award
-
-
 
 
 
