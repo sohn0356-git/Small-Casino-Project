bea46094803d6accd931b6710812f6aa2b591b3e
sohn0356-git <sohn0356@gmail.com>
2021-01-09 15:34:22 +0900

    Update Small_Casino_Project.py

--- Small Casino Project/Small_Casino_Project.py
+++ Small Casino Project/Small_Casino_Project.py
@@ -4,11 +4,41 @@ import Hangman
 import NumberGuessGame
 
 
+user_money = 5
 
-def game_start():
-    user_money = 5
+def start_game():
+    global user_money
+    """
+        사용자에게 다음과 같은 선택지를 제시합니다.
+        1. Play Blackjack Game
+        2. Play Bulls and Cows Game
+        3. Play Hangman Game
+        4. Play Number Guess Game
+        5. Quit Casino
+    """
+
     while True:
-        print("현재 당신의 재산은 %d천만원입니다!")
+        award = 0
+        menu = input("무엇을 하시겠습니까? \n\n1. Play Blackjack Game\n2. Play Bulls and Cows Game\n3. Play Hangman Game\n4. Play Number Guess Game\n5. Quit Casino\n")
+
+        if menu == "1":
+            award = Blackjack.play()
+        elif menu == "2":
+            award = Bulls_and_Cows.play()
+        elif menu == "3":
+            award = Hangman.play()
+        elif menu == "4":
+            award = NumberGuessGame.play()
+        elif menu == "5":
+            print("무엇이든 적당히 할 때 가장 즐거운 법입니다. 좋은 시간이셨기를 바랍니다")
+            break
+        else:
+            print("다시 입력해주세요 : ")
+            continue
+        user_money += award
+        print("\n\n\n")
+        print("현재 당신의 재산은 %d천만원입니다!" % user_money)
+
 
 
 def main():
@@ -18,7 +48,8 @@ def main():
         5가지 게임이 존재하고 추후에 더 추가될 수도 있다.
     """
     print("1조의 Small Casino에 오신 것을 환영합니다. 즐거운 시간 되세요~")
-    game_start()
+    print("현재 당신의 재산은 %d천만원입니다!" % user_money)
+    start_game()
 
 
 
